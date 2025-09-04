b97affe2e07f4eb6ecb240b5f662195150784b35
GhosTHaise <ghostrex2@gmail.com>
2025-09-04 19:33:08 +0300

    feat : use latest version of crawl4ai

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from crawl4ai import WebCrawler
+import asyncio
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
 
-# Create an instance of WebCrawler
-crawler = WebCrawler()
+async def main():
+    browser_conf = BrowserConfig(headless=True)  # or False to see the browser
+    run_conf = CrawlerRunConfig(
+        cache_mode=CacheMode.BYPASS
+    )
 
-# Warm up the crawler (load necessary models)
-crawler.warmup()
+    async with AsyncWebCrawler(config=browser_conf) as crawler:
+        result = await crawler.arun(
+            url="https://example.com",
+            config=run_conf
+        )
+        print(result.markdown)
 
-# Run the crawler on a URL
-result = crawler.run(url="https://openai.com/api/pricing/")
-
-# Print the extracted contents
-print(result.markdown)
+if __name__ == "__main__":
+    asyncio.run(main())
